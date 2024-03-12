48e123b828829c72e5cd3265f1a46cea0e9a9582
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-03-12 11:10:14 -0700

    Initial commit.

---
+++ resources/js/pages/Tool.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Head title="Pulse" />
+    <Card
+      class="flex flex-col items-center justify-center"
+      style="min-height: 100vh"
+    >
+
+    <iframe width="100%" height="900px" src="/pulse" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    //
+  },
+}
+</script>
+
+<style>
+/* Scoped Styles */
+</style>
